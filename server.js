354999f6c7e6cd72e14cf66063355b6f6de4ca4f
EkaterinaSukhanova <katrinsuh@yandex.ru>
2018-09-05 15:15:53 +0300

    don't touch

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { shopRoute, commentsRoute } = require('./routes')
+const { PORT = 80 } = process.env
+
+const app = express()
+
+app.use((req, res, next) => {
+  //настройка корс запросов на сервере (3 заголовка)
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+  res.header('Content-Type', 'application/json')
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/shop', shopRoute)
+app.use('/comments', commentsRoute)
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
